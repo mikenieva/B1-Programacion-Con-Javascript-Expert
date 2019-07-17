a796b419153e9dcc618e191f6d1e0ec9e004b8c9
Mike Nieva <m@mikenieva.com>
2019-07-17 06:18:07 -0500

    Create index.js

---
+++ sesion3-es6/Ejemplo-03/index.js
@@ -0,0 +1,6 @@
+const nombre = ‘Mike’
+const mensaje = 
+`Hola, mi nombre es ${nombre}`
+const titulo = ‘<h1>Hola mundo</h1>’ 
+const mensaje = `${titulo}`
+const suma = `${3*3} es el resultado de 3 * 3`
