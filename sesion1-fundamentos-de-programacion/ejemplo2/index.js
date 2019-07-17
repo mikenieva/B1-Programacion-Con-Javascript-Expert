3c922cf3881ecf8a6010a6721ddef19744203679
Mike Nieva <m@mikenieva.com>
2019-07-17 06:08:08 -0500

    Create index.js

---
+++ sesion1-fundamentos-de-programacion/ejemplo2/index.js
@@ -0,0 +1,11 @@
+var adicion = 4 + 3;
+var resta = 4 - 3;
+var multiplicacion = 4 * 3;
+var division = 4 / 3;
+var resultado = 5 % 2;
+
+var incremento = 4;
+incremento++;
+
+var decremento = 4;
+decremento--;
