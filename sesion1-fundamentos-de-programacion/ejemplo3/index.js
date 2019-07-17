1fb58489a3b26eb8f29c98cea5001824aa7343af
Mike Nieva <m@mikenieva.com>
2019-07-17 06:08:47 -0500

    Create index.js

---
+++ sesion1-fundamentos-de-programacion/ejemplo3/index.js
@@ -0,0 +1,11 @@
+var now = 13
+
+if(now < 12){
+  console.log("Buenos días")
+} else if(now >= 12 && now <= 19 ){
+  console.log("Buenas tardes")
+} else if(now > 19){
+  console.log("Buenas noches")
+} else {
+    console.log("No entiendo que hora es")
+}
