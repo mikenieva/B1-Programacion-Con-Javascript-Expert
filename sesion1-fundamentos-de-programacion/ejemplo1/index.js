f362728d222efb143e210669d6291c011a101072
Mike Nieva <m@mikenieva.com>
2019-07-17 06:07:21 -0500

    Create index.js

---
+++ sesion1-fundamentos-de-programacion/ejemplo1/index.js
@@ -0,0 +1,31 @@
+// STRING - Texto plano
+var fruta = "manzana";
+var fraseDesayuno = "Hoy comeré 3 manzanas";
+
+// NUMBER - Número
+var edad = 29;
+var diasExpiracionManzana = 7;
+
+// BOOLEAN (Booleano) - Valor vinculado al "verdadero" o "falso"
+var esComestible = true;
+var esRoja = false;
+
+// UNDEFINED - Valor que tiene una variable a la cual no se le ha asignado ningún valor (aún)
+var fechaDesayuno
+// ó...
+var fechaDesayuno = undefined;
+
+// NULL - La variable no tiene ningún valor. Puede referirse a un "no aplica".
+var cascaraManzana = null;
+
+// ARREGLO - Conjunto de datos. Cada lugar en el arreglo puede ser otro tipo de dato.
+var desayuno = ["Leche", "Manzana"];
+var invitados = ["Mike", "Andrés", "Salvador"];
+
+// OBJETO - Conjunto de datos, el cual contiene se estructuran en un formato llave:valor 
+
+var frutas = {
+    nombre: "manzana",
+    color: "roja",
+    esComestible: true   
+}
