4b110e513a0f474cc6b7c4ed7000d2f45b7d421a
Mike Nieva <m@mikenieva.com>
2019-07-17 06:15:26 -0500

    Create index.js

---
+++ sesion3-es6/Ejemplo-01/index.js
@@ -0,0 +1,12 @@
+let a = 10;
+a = 20
+console.log(a)    // 20
+const b = 10
+b = 20    // Error
+const auto = {
+  color: “rojo”
+}
+auto.color = “azul” // Éxito
+auto = 4 // Error
+const nombres = [“mike”, “salvador”]
+nombres[0] = “andres” // Éxito
