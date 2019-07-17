bf8446df76b073692ecb2365c3d508da977e95fa
Mike Nieva <m@mikenieva.com>
2019-07-17 06:17:45 -0500

    Create index.js

---
+++ sesion3-es6/Ejemplo-02/index.js
@@ -0,0 +1,12 @@
+function numero ( ) {
+   let i = 30
+   console.log(i)
+}
+numero ( )   // 30
+let i = 10
+function numero ( ) {
+   let i = 30
+   console.log(i)
+}
+numero( ) // 30
+console.log(i) // 10 
