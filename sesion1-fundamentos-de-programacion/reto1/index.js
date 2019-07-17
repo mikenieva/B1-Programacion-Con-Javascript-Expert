b53684954ddea9975164d9691355d925726ca1ad
Mike Nieva <m@mikenieva.com>
2019-07-17 08:31:50 -0500

    Create index.js

---
+++ sesion1-fundamentos-de-programacion/reto1/index.js
@@ -0,0 +1,14 @@
+var nombre = "Mike"
+var edad = 31
+var hacesEjercicio = true
+var indefinido
+var aprenderAProgramar = null
+
+var carro = {
+    color: "red",
+    llantas: 4,
+    marca: "VW",
+    tieneMotor: true
+}
+
+var empresas = ["Apple", "Microsoft", "Google"]
